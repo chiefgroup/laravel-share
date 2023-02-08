c89e846a31ca6f31a6b090ac253692d96aebab7d
Observer <sxpd7788+g@gmail.com>
2023-02-08 16:10:36 +0800

    更新注释

--- src/OssServiceFacade.php
+++ src/OssServiceFacade.php
@@ -7,7 +7,10 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class OssServiceFacade
  *
- * @method static void getAccessUrl(string $bucket, string $object, int $timeout = 600);
+ * @method static void getAccessUrl(string $object, int $timeout = 600);
+ * @method static void getObjectUrl(string $object);
+ * @method static void deleteObject(string $object);
+ * @method static void objectAcl(string $object, string $acl)
  */
 class OssServiceFacade extends Facade
 {
