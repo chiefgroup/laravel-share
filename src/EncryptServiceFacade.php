a707568f3ffc734a68267e953185873378e9a5c6
erpeng <2512422541@qq.com>
2023-05-08 10:19:48 +0800

    EncryptService add method.

--- src/EncryptServiceFacade.php
+++ src/EncryptServiceFacade.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Facade;
  *
  * @method static string decrypt(string $str)
  * @method static string encrypt(string $str)
+ * @method static string signGenerate(string $timestamp, string $secret)
+ * @method static bool signVerify(string $timestamp, string $secret, string $sign)
  */
 class EncryptServiceFacade extends Facade
 {
