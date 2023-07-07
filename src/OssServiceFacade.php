43871fa25e6d1c26faaec8fa859497e1875bf2ba
erpeng <2512422541@qq.com>
2023-07-07 16:09:07 +0800

    add oss getSignedUrlObject.

--- src/OssServiceFacade.php
+++ src/OssServiceFacade.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static void deleteObject(string $object);
  * @method static void objectAcl(string $object, string $acl)
  * @method static string getCustomDomainUrl(string $object)
+ * @method static string|array getSignedUrlObject($object, int $timeout = 3600, bool $flag = false)
  */
 class OssServiceFacade extends Facade
 {
