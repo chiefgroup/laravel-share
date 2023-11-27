57362d76ecf73dc5e8a30e9b82ec1f696461005e
erpeng <2512422541@qq.com>
2023-11-27 17:31:23 +0800

    NewDingtalkService add requestSync method.

--- src/NewDingtalkServiceFacade.php
+++ src/NewDingtalkServiceFacade.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Facade;
  * @package App\Facade
  *
  * @method static mixed request(string $appName, string $methods, string $uri, array $options)
+ * @method static mixed requestSync(string $appName, string $methods, string $uri, array $options)
  * @method static string getToken(string $appName)
  * @method static mixed robotSend(array $message, string $accessToken, string $secret = null)
  */
