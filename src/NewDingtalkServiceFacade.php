f3aed7aa7066dc1951fc0b209614aefcf11cd2c8
erpeng <2512422541@qq.com>
2023-12-02 16:46:30 +0800

    DingtalkServiceFacade Method adj.

--- src/NewDingtalkServiceFacade.php
+++ src/NewDingtalkServiceFacade.php
@@ -8,10 +8,11 @@ use Illuminate\Support\Facades\Facade;
  * Class NewDingtalkService
  * @package App\Facade
  *
+ * @method static mixed getConfig(string $appName)
  * @method static mixed request(string $appName, string $methods, string $uri, array $options)
  * @method static mixed requestOther(string $methods, string $uri, array $options, string $accessToken = '')
- * @method static string getToken(string $appName)
- * @method static mixed robotSend(array $message, string $accessToken, string $secret = null)
+ * @method static string getAccessToken(string $appName)
+ * @method static mixed robotGroupMessagesSend(string $appName, string $conversationKey, array $options)
  */
 class NewDingtalkServiceFacade extends Facade
 {
