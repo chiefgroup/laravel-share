1b99590fca4635c02187977ab127b2eb90e8eb4f
erpeng <2512422541@qq.com>
2023-12-07 17:24:33 +0800

    DingtalkService getConfig  Method adj.

--- src/NewDingtalkServiceFacade.php
+++ src/NewDingtalkServiceFacade.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade;
  * Class NewDingtalkService
  * @package App\Facade
  *
- * @method static mixed getConfig(string $appName)
+ * @method static mixed getConfig(string $appName = null)
  * @method static mixed request(string $appName, string $methods, string $uri, array $options)
  * @method static mixed requestOther(string $methods, string $uri, array $options, string $accessToken = '')
  * @method static string getAccessToken(string $appName)
