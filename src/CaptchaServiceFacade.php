a1309897517f7b71bc3febe7a823105f90e3ebe4
Observer <sxpd7788+g@gmail.com>
2024-03-04 10:47:13 +0800

    fix

--- src/CaptchaServiceFacade.php
+++ src/CaptchaServiceFacade.php
@@ -4,6 +4,9 @@ namespace ChiefGroup\LaravelShare;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static traceless(string $captchaVerifyParam, string $scene = 'web')
+ */
 class CaptchaServiceFacade extends Facade
 {
     protected static function getFacadeAccessor()
