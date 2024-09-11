9a31b7cd28f95777f806676e68bedd78b21b0989
peng <2512422541@qq.com>
2024-09-11 11:28:46 +0800

    fix JuShuiTanService rpc.

--- src/JuShuiTanServiceFacade.php
+++ src/JuShuiTanServiceFacade.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\Facade;
  * @package App\Facade
  *
  * @method static string getInitToken()
+ * @method static string getConfig()
+ * @method static string|null getSign(string $appSecret, ?array $data)
+ * @method static string|null getSignByData(?array $data)
  * @method static mixed inventoryQuery(array $params = [], ?string $accessToken = null)
  * @method static mixed request(string $methods, string $uri, array $options = [])
  */
